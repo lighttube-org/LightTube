c49aed7264462b5871385ce89de12fc90cf91b7b
Kayra Uylar <k.uylar@outlook.com>
2022-05-04 22:47:51 +0300

    Tidy up database classes

--- LightTube/Database/DatabaseManager.cs
+++ LightTube/Database/DatabaseManager.cs
@@ -1,17 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Web;
-using System.Xml;
-using Humanizer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
-using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
-using MyCSharp.HttpUserAgentParser;
 using Newtonsoft.Json;
 
 namespace LightTube.Database
@@ -130,113 +122,6 @@ namespace LightTube.Database
 
 			user = null;
 			return false;
-		}
-	}
-
-	[BsonIgnoreExtraElements]
-	public class LTUser
-	{
-		public string Email;
-		public string PasswordHash;
-		public List<string> SubscribedChannels;
-		public bool ApiAccess;
-	}
-
-	[BsonIgnoreExtraElements]
-	public class LTLogin
-	{
-		public string Identifier;
-		public string Email;
-		public string Token;
-		public string UserAgent;
-		public string[] Scopes;
-		public DateTimeOffset Created = DateTimeOffset.MinValue;
-		public DateTimeOffset LastSeen = DateTimeOffset.MinValue;
-
-		public XmlDocument GetXmlElement()
-		{
-			XmlDocument doc = new();
-			XmlElement login = doc.CreateElement("Login");
-			login.SetAttribute("id", Identifier);
-			login.SetAttribute("user", Email);
-
-			XmlElement token = doc.CreateElement("Token");
-			token.InnerText = Token;
-			login.AppendChild(token);
-
-			XmlElement scopes = doc.CreateElement("Scopes");
-			foreach (string scope in Scopes)
-			{
-				XmlElement scopeElement = doc.CreateElement("Scope");
-				scopeElement.InnerText = scope;
-				login.AppendChild(scopeElement);
-			}
-			login.AppendChild(scopes);
-			
-			doc.AppendChild(login);
-			return doc;
-		}
-
-		public string GetTitle()
-		{
-			Match match = Regex.Match(UserAgent, DatabaseManager.ApiUaRegex);
-			if (match.Success)
-				return $"API App: {match.Groups[2]} {match.Groups[3]}";
-
-			HttpUserAgentInformation client = HttpUserAgentParser.Parse(UserAgent);
-			StringBuilder sb = new($"{client.Name} {client.Version}");
-			if (client.Platform.HasValue)
-				sb.Append($" on {client.Platform.Value.PlatformType.ToString()}");
-			return sb.ToString();
-		}
-
-		public string GetDescription()
-		{
-			StringBuilder sb = new();
-			sb.AppendLine($"Created: {Created.Humanize(DateTimeOffset.Now)}");
-			sb.AppendLine($"Last seen: {LastSeen.Humanize(DateTimeOffset.Now)}");
-
-			Match match = Regex.Match(UserAgent, DatabaseManager.ApiUaRegex);
-			if (match.Success)
-			{
-				sb.AppendLine($"API version: {HttpUtility.HtmlEncode(match.Groups[1])}");
-				sb.AppendLine($"App info: {HttpUtility.HtmlEncode(match.Groups[4])}");
-				sb.AppendLine("Allowed scopes:");
-				foreach (string scope in Scopes) sb.AppendLine($"- {scope}");
-			}
-
-			return sb.ToString();
-		}
-
-		public async Task UpdateLastAccess(DateTimeOffset newTime)
-		{
-			await DatabaseManager.Logins.UpdateLastAccess(Identifier, newTime);
-		}
-	}
-
-	[BsonIgnoreExtraElements]
-	public class LTChannel
-	{
-		public string ChannelId;
-		public string Name;
-		public string Subscribers;
-		public string IconUrl;
-
-		public XmlNode GetXmlElement(XmlDocument doc)
-		{
-			XmlElement item = doc.CreateElement("Channel");
-			item.SetAttribute("id", ChannelId);
-			item.SetAttribute("subscribers", Subscribers);
-
-			XmlElement title = doc.CreateElement("Name");
-			title.InnerText = Name;
-			item.AppendChild(title);
-
-			XmlElement thumbnail = doc.CreateElement("Avatar");
-			thumbnail.InnerText = IconUrl;
-			item.AppendChild(thumbnail);
-
-			return item;
 		}
 	}
 }
---
+++ LightTube/Database/LTUser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LightTube.Database
+{
+	[BsonIgnoreExtraElements]
+	public class LTUser
+	{
+		public string Email;
+		public string PasswordHash;
+		public List<string> SubscribedChannels;
+		public bool ApiAccess;
+	}
+}
