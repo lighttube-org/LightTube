18adbeb37e24b45386425398da064f81bb0740a0
Kayra Uylar <k.uylar@outlook.com>
2022-12-07 20:19:24 +0300

    Clean channel cache at startup

--- LightTube/Database/Database.cs
+++ LightTube/Database/Database.cs
@@ -7,14 +7,18 @@ namespace LightTube.Database;
 public class Database
 {
 	private static IMongoCollection<DatabaseUser> _userCollection;
+	private static IMongoCollection<DatabaseChannel> _channelCacheCollection;
 
 	public static void Init(string connstr)
 	{
 		MongoClient client = new(connstr);
 		IMongoDatabase database = client.GetDatabase("lighttube");
 		_userCollection = database.GetCollection<DatabaseUser>("users");
+		_channelCacheCollection = database.GetCollection<DatabaseChannel>("channelCache");
+
 		
 		RunMigrations();
+		ClearJunkData();
 	}
 
 	public static async void RunMigrations()
@@ -32,5 +36,30 @@ public class Database
 				Console.WriteLine("Migrated: " + (replaceAsync.UserID ?? "{!}" + replaceAsync.Email));
 			}
 		}
+	}
+
+	public static async void ClearJunkData()
+	{
+		List<string> channels = new();	
+		IAsyncCursor<DatabaseUser> allUsers = await _userCollection.FindAsync(x => true);
+		while (await allUsers.MoveNextAsync())
+			foreach (DatabaseUser user in allUsers.Current)
+				foreach (string channel in user.Subscriptions.Keys)
+					if (!channels.Contains(channel))
+						channels.Add(channel);
+
+		IMongoQueryable<DatabaseChannel> cachedChannels = _channelCacheCollection.AsQueryable();
+		int deletedChannels = 0;
+		foreach (DatabaseChannel channel in cachedChannels)
+		{
+			if (!channels.Contains(channel.ChannelId))
+			{
+				await _channelCacheCollection.DeleteOneAsync(x => x.ChannelId == channel.ChannelId);
+				deletedChannels++;
+				if (deletedChannels % 5000 == 0)
+					Console.WriteLine($"Deleted {deletedChannels} channels from the cache, and still going on...");
+			}
+		}
+		Console.WriteLine($"Deleted {deletedChannels} channels from the cache");
 	}
 }
---
+++ LightTube/Database/Models/DatabaseChannel.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace LightTube.Database.Models;
+
+[BsonIgnoreExtraElements]
+public class DatabaseChannel
+{
+	public string ChannelId;
+	public string Name;
+	public string Subscribers;
+	public string IconUrl;
+}
