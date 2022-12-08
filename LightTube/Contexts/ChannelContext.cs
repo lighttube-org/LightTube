57b6ab979c84311fc86c3e23c92850d61c41f9db
Kayra Uylar <k.uylar@outlook.com>
2022-12-08 23:55:18 +0300

    Remove unneeded tabs from the channel page

--- LightTube/Contexts/ChannelContext.cs
+++ LightTube/Contexts/ChannelContext.cs
@@ -10,6 +10,7 @@ public class ChannelContext : BaseContext
 	public IEnumerable<IRenderer> Content;
 	public string Id;
 	public string? Continuation;
+	public ChannelTabs[] Tabs;
 
 	public ChannelContext(HttpContext context, ChannelTabs tab, InnerTubeChannelResponse channel, string id) : base(context)
 	{
@@ -19,6 +20,7 @@ public class ChannelContext : BaseContext
 		Content = channel.Contents;
 		Continuation =
 			(channel.Contents.FirstOrDefault(x => x is ContinuationItemRenderer) as ContinuationItemRenderer)?.Token;
+		Tabs = channel.EnabledTabs;
 	}
 
 	public ChannelContext(HttpContext context, ChannelTabs tab, InnerTubeContinuationResponse continuation, string id) : base(context)
@@ -27,5 +29,6 @@ public class ChannelContext : BaseContext
 		CurrentTab = tab;
 		Content = continuation.Contents;
 		Continuation = continuation.Continuation;
+		Tabs = Enum.GetValues<ChannelTabs>();
 	}
 }
