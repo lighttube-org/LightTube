7475a2409a3e46f5f3af86c91f0b34af68882d44
Kayra Uylar <k.uylar@outlook.com>
2023-05-29 23:30:49 +0300

    Fix #46

--- LightTube/Contexts/ChannelContext.cs
+++ LightTube/Contexts/ChannelContext.cs
@@ -43,6 +43,25 @@ public class ChannelContext : BaseContext
 		AddMeta("og:url", $"{context.Request.Scheme}://{context.Request.Host}/{context.Request.Path}{context.Request.QueryString}");
 		AddMeta("og:image", channel.Header?.Avatars.Last().Url.ToString() ?? "");
 		AddMeta("twitter:card", channel.Header?.Avatars.Last().Url.ToString() ?? "");
+
+		if (channel.Contents.Any(x => x is ChannelVideoPlayerRenderer || x is ItemSectionRenderer isr && isr.Contents.Any(y => y is ChannelVideoPlayerRenderer)))
+		{
+			AddStylesheet("/lib/videojs/video-js.min.css");
+			AddStylesheet("/lib/videojs-endscreen/videojs-endscreen.css");
+			AddStylesheet("/lib/videojs-vtt-thumbnails/videojs-vtt-thumbnails.min.css");
+			AddStylesheet("/lib/videojs-hls-quality-selector/videojs-hls-quality-selector.css");
+			AddStylesheet("/lib/silvermine-videojs-quality-selector/silvermine-videojs-quality-selector.css");
+			AddStylesheet("/css/vjs-skin.css");
+
+			AddScript("/lib/videojs/video.min.js");
+			AddScript("/lib/videojs-hotkeys/videojs.hotkeys.min.js");
+			AddScript("/lib/videojs-endscreen/videojs-endscreen.js");
+			AddScript("/lib/videojs-vtt-thumbnails/videojs-vtt-thumbnails.min.js");
+			AddScript("/lib/videojs-contrib-quality-levels/videojs-contrib-quality-levels.min.js");
+			AddScript("/lib/videojs-hls-quality-selector/videojs-hls-quality-selector.min.js");
+			AddScript("/lib/silvermine-videojs-quality-selector/silvermine-videojs-quality-selector.min.js");
+			AddScript("/js/player.js");
+		}
 	}
 
 	public ChannelContext(HttpContext context, ChannelTabs tab, InnerTubeChannelResponse channel, InnerTubeContinuationResponse continuation, string id) : base(context)
