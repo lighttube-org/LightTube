8614724489e3958e93d09526de287b8c6fe01819
Kayra Uylar <52961639+kuylar@users.noreply.github.com>
2023-05-30 18:57:16 +0300

    Api and quality selector fix (#48)
    - Make /api/subscriptions return channel objects
    - Add quality switcher for no-JS users

--- LightTube/Contexts/PlayerContext.cs
+++ LightTube/Contexts/PlayerContext.cs
@@ -14,14 +14,16 @@ public class PlayerContext : BaseContext
 	public bool UseEmbedUi = false;
 	public string? ClassName;
 
-	public PlayerContext(HttpContext context, InnerTubePlayer innerTubePlayer, string className, bool compatibility) : base(context)
+	public PlayerContext(HttpContext context, InnerTubePlayer innerTubePlayer, string className, bool compatibility,
+		string preferredItag) : base(context)
 	{
 		Player = innerTubePlayer;
 		ClassName = className;
+		PreferredItag = preferredItag;
 		UseHls = innerTubePlayer.DashManifestUrl is not null && !compatibility; // Prefer HLS if the video is live
-																			// Live videos contain a DASH manifest URL
-		UseDash = innerTubePlayer.AdaptiveFormats.Any() && !compatibility;  // Prefer DASH if we can provide Adaptive
-																			// Formats
+		// Live videos contain a DASH manifest URL
+		UseDash = innerTubePlayer.AdaptiveFormats.Any() && !compatibility; // Prefer DASH if we can provide Adaptive
+		// Formats
 		if (Configuration.GetVariable("LIGHTTUBE_DISABLE_PROXY", "false") != "false")
 		{
 			UseHls = false;
@@ -34,7 +36,16 @@ public class PlayerContext : BaseContext
 		Exception = e;
 	}
 
-	public string? GetFirstItag() => Player?.Formats.First(x => x.Itag == PreferredItag && x.Itag != "17").Itag;
+	public string? GetFirstItag() => GetPreferredFormat()?.Itag;
+
+	public Format? GetPreferredFormat() =>
+		Player?.Formats.FirstOrDefault(x => x.Itag == PreferredItag && x.Itag != "17") ??
+		Player?.Formats.First(x => x.Itag != "17");
 
 	public string GetClass() => ClassName is not null ? $" {ClassName}" : "";
+
+	public IEnumerable<Format> GetFormatsInPreferredOrder()
+	{
+		return Player!.Formats.OrderBy(x => x.Itag != PreferredItag);
+	}
 }
