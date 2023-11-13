dd16622ab478805ae99887f726e9f3b98a69f6ad
Kayra Uylar <k.uylar@outlook.com>
2023-11-13 13:15:04 +0300

    Add timestamp links

--- LightTube/wwwroot/js/player.js
+++ LightTube/wwwroot/js/player.js
@@ -11,3 +11,12 @@ if (oldVol) {
 }
 
 const player = new Player(`video#${playerId}`, videoInfo, playtype);
+
+document.querySelectorAll("a[href*='?v="+videoId+"&t=']").forEach(x => {
+	const t = Number(x.getAttribute("href").split("t=")[1].replace(/[^0-9]/, ""));
+	x.onclick =  e => {
+		e.preventDefault();
+		player.player.currentTime = t;
+		player.player.scrollIntoView({behavior: "smooth"});
+	};
+});
