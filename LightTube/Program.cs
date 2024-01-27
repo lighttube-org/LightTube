87dbc5f3482f88be4460fbce8e0e4ed6a0e3de4b
InF <inftord@inftord.tech>
2024-01-27 17:50:49 +0500

    Implement Serilog logging (#118)
    * add serilog
    
    * do a little bit more complicated logging setup
    
    * delete MigrateDataChore.cs because it's not used
    
    * replace console.writeline with serilog log.debug/error

--- LightTube/Program.cs
+++ LightTube/Program.cs
@@ -3,51 +3,79 @@ using InnerTube;
 using LightTube;
 using LightTube.Chores;
 using LightTube.Database;
+using Serilog;
+using Serilog.Events;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
 
 // Add services to the container.
-builder.Services.AddControllersWithViews().AddNewtonsoftJson();
+    builder.Services.AddControllersWithViews().AddNewtonsoftJson();
 
-InnerTubeAuthorization? auth = Configuration.GetInnerTubeAuthorization();
-builder.Services.AddSingleton(new InnerTube.InnerTube(new InnerTubeConfiguration
-{
-	Authorization = auth,
-	CacheSize = int.Parse(Configuration.GetVariable("LIGHTTUBE_CACHE_SIZE", "50")!),
-	CacheExpirationPollingInterval = default
-}));
-builder.Services.AddSingleton(new HttpClient());
+    InnerTubeAuthorization? auth = Configuration.GetInnerTubeAuthorization();
+    builder.Services.AddSingleton(new InnerTube.InnerTube(new InnerTubeConfiguration
+    {
+        Authorization = auth,
+        CacheSize = int.Parse(Configuration.GetVariable("LIGHTTUBE_CACHE_SIZE", "50")!),
+        CacheExpirationPollingInterval = default
+    }));
+    builder.Services.AddSingleton(new HttpClient());
 
-await JsCache.DownloadLibraries();
-ChoreManager.RegisterChores();
-DatabaseManager.Init(Configuration.GetVariable("LIGHTTUBE_MONGODB_CONNSTR"));
+    await JsCache.DownloadLibraries();
+    ChoreManager.RegisterChores();
+    DatabaseManager.Init(Configuration.GetVariable("LIGHTTUBE_MONGODB_CONNSTR"));
 
-WebApplication app = builder.Build();
+    WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
-	app.UseHttpsRedirection();
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+        app.UseHttpsRedirection();
+    }
+
+    app.UseStaticFiles(new StaticFileOptions
+    {
+        OnPrepareResponse = ctx =>
+        {
+            // Cache static files for 3 days
+            ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=259200");
+            ctx.Context.Response.Headers.Append("Expires",
+                DateTime.UtcNow.AddDays(3).ToString("R", CultureInfo.InvariantCulture));
+        }
+    });
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseStaticFiles(new StaticFileOptions {
-	OnPrepareResponse = ctx =>
-	{
-		// Cache static files for 3 days
-		ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=259200");
-		ctx.Context.Response.Headers.Append("Expires", DateTime.UtcNow.AddDays(3).ToString("R", CultureInfo.InvariantCulture));
-	}
-});
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
