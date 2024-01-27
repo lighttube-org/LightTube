610efee565c029c06540e066dd8f2078757c4627
InFTord <inftord@inftord.tech>
2024-01-27 15:09:15 +0500

    add serilog

--- LightTube/Program.cs
+++ LightTube/Program.cs
@@ -3,51 +3,74 @@ using InnerTube;
 using LightTube;
 using LightTube.Chores;
 using LightTube.Database;
+using Serilog;
+using Serilog.Events;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews().AddNewtonsoftJson();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .CreateLogger();
 
-InnerTubeAuthorization? auth = Configuration.GetInnerTubeAuthorization();
-builder.Services.AddSingleton(new InnerTube.InnerTube(new InnerTubeConfiguration
+try
 {
-	Authorization = auth,
-	CacheSize = int.Parse(Configuration.GetVariable("LIGHTTUBE_CACHE_SIZE", "50")!),
-	CacheExpirationPollingInterval = default
-}));
-builder.Services.AddSingleton(new HttpClient());
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+    
+    builder.Host.UseSerilog();
+ 
+// Add services to the container.
+    builder.Services.AddControllersWithViews().AddNewtonsoftJson();
 
-await JsCache.DownloadLibraries();
-ChoreManager.RegisterChores();
-DatabaseManager.Init(Configuration.GetVariable("LIGHTTUBE_MONGODB_CONNSTR"));
+    InnerTubeAuthorization? auth = Configuration.GetInnerTubeAuthorization();
+    builder.Services.AddSingleton(new InnerTube.InnerTube(new InnerTubeConfiguration
+    {
+        Authorization = auth,
+        CacheSize = int.Parse(Configuration.GetVariable("LIGHTTUBE_CACHE_SIZE", "50")!),
+        CacheExpirationPollingInterval = default
+    }));
+    builder.Services.AddSingleton(new HttpClient());
 
-WebApplication app = builder.Build();
+    await JsCache.DownloadLibraries();
+    ChoreManager.RegisterChores();
+    DatabaseManager.Init(Configuration.GetVariable("LIGHTTUBE_MONGODB_CONNSTR"));
+
+    WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
-	app.UseHttpsRedirection();
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+        app.UseHttpsRedirection();
+    }
+
+    app.UseStaticFiles(new StaticFileOptions
+    {
+        OnPrepareResponse = ctx =>
+        {
+            // Cache static files for 3 days
+            ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=259200");
+            ctx.Context.Response.Headers.Append("Expires",
+                DateTime.UtcNow.AddDays(3).ToString("R", CultureInfo.InvariantCulture));
+        }
+    });
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseStaticFiles(new StaticFileOptions {
-	OnPrepareResponse = ctx =>
-	{
-		// Cache static files for 3 days
-		ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=259200");
-		ctx.Context.Response.Headers.Append("Expires", DateTime.UtcNow.AddDays(3).ToString("R", CultureInfo.InvariantCulture));
-	}
-});
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
