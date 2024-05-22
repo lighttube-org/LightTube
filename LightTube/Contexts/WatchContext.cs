f4b9d3b5242491b18786fc4d71221b68beb94b2a
Kayra Uylar <k.uylar@outlook.com>
2024-05-22 10:25:48 +0300

    Update YoutubeController.cs

--- LightTube/Contexts/WatchContext.cs
+++ LightTube/Contexts/WatchContext.cs
@@ -7,149 +7,149 @@ namespace LightTube.Contexts;
 
 public class WatchContext : BaseContext
 {
-    public PlayerContext Player;
-    public InnerTubeVideo Video;
-    public VideoPlaylistInfo? Playlist;
-    public ContinuationResponse? Comments;
-    public int Dislikes;
-    public int Likes;
-    public SponsorBlockSegment[] Sponsors;
+	public PlayerContext Player;
+	public InnerTubeVideo Video;
+	public VideoPlaylistInfo? Playlist;
+	public ContinuationResponse? Comments;
+	public int Dislikes;
+	public int Likes;
+	public SponsorBlockSegment[] Sponsors;
 
-    public WatchContext(HttpContext context, InnerTubePlayer innerTubePlayer, InnerTubeVideo innerTubeVideo,
-        ContinuationResponse? comments, bool compatibility, int dislikes, int likes,
-        SponsorBlockSegment[] sponsors) : base(context)
-    {
-        Player = new PlayerContext(context, innerTubePlayer, innerTubeVideo, "embed", compatibility,
-            context.Request.Query["q"], sponsors);
-        Video = innerTubeVideo;
-        Playlist = Video.Playlist;
-        Comments = comments;
-        Dislikes = dislikes;
-        Likes = likes;
-        Sponsors = sponsors;
-        GuideHidden = true;
+	public WatchContext(HttpContext context, InnerTubePlayer innerTubePlayer, InnerTubeVideo innerTubeVideo,
+		ContinuationResponse? comments, bool compatibility, int dislikes,
+		SponsorBlockSegment[] sponsors) : base(context)
+	{
+		Player = new PlayerContext(context, innerTubePlayer, innerTubeVideo, "embed", compatibility,
+			context.Request.Query["q"], sponsors);
+		Video = innerTubeVideo;
+		Playlist = Video.Playlist;
+		Comments = comments;
+		Dislikes = dislikes;
+		Likes = -1; // todo: value parsers
+		Sponsors = sponsors;
+		GuideHidden = true;
 
-        AddMeta("description", Video.Description);
-        AddMeta("author", Video.Channel.Title);
-        AddMeta("og:title", Video.Title);
-        AddMeta("og:description", Video.Description);
-        AddMeta("og:url",
-            $"{context.Request.Scheme}://{context.Request.Host}/{context.Request.Path}{context.Request.QueryString}");
-        AddMeta("og:image", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
-        AddMeta("og:video:url", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
-        AddMeta("og:video:width", "640");
-        AddMeta("og:video:height", "360");
-        AddMeta("og:type", "video.other");
-        AddMeta("twitter:card", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
-        AddMeta("twitter:player", $"https://{context.Request.Host}/embed/{Video.Id}");
-        AddMeta("twitter:player:stream", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
+		AddMeta("description", Video.Description);
+		AddMeta("author", Video.Channel.Title);
+		AddMeta("og:title", Video.Title);
+		AddMeta("og:description", Video.Description);
+		AddMeta("og:url",
+			$"{context.Request.Scheme}://{context.Request.Host}/{context.Request.Path}{context.Request.QueryString}");
+		AddMeta("og:image", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
+		AddMeta("og:video:url", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
+		AddMeta("og:video:width", "640");
+		AddMeta("og:video:height", "360");
+		AddMeta("og:type", "video.other");
+		AddMeta("twitter:card", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
+		AddMeta("twitter:player", $"https://{context.Request.Host}/embed/{Video.Id}");
+		AddMeta("twitter:player:stream", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
 
-        AddStylesheet("/lib/ltplayer.css");
+		AddStylesheet("/lib/ltplayer.css");
 
-        AddScript("/lib/ltplayer.js");
-        AddScript("/lib/hls.js");
-        AddScript("/js/player.js");
-    }
+		AddScript("/lib/ltplayer.js");
+		AddScript("/lib/hls.js");
+		AddScript("/js/player.js");
+	}
 
-    public WatchContext(HttpContext context, Exception e, InnerTubeVideo innerTubeVideo, ContinuationResponse? comments,
-        int dislikes, int likes) : base(context)
-    {
-        Player = new PlayerContext(context, e);
-        Video = innerTubeVideo;
-        Playlist = Video.Playlist;
-        Comments = comments;
-        Dislikes = dislikes;
-        Likes = likes;
-        Sponsors = [];
-        GuideHidden = true;
+	public WatchContext(HttpContext context, Exception e, InnerTubeVideo innerTubeVideo, ContinuationResponse? comments,
+		int dislikes) : base(context)
+	{
+		Player = new PlayerContext(context, e);
+		Video = innerTubeVideo;
+		Playlist = Video.Playlist;
+		Comments = comments;
+		Dislikes = dislikes;
+		Likes = -1; // todo: value parsers
+		Sponsors = [];
+		GuideHidden = true;
 
-        AddMeta("description", Video.Description);
-        AddMeta("author", Video.Channel.Title);
-        AddMeta("og:title", Video.Title);
-        AddMeta("og:description", Video.Description);
-        AddMeta("og:url",
-            $"{context.Request.Scheme}://{context.Request.Host}/{context.Request.Path}{context.Request.QueryString}");
-        AddMeta("og:image", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
-        AddMeta("og:video:url", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
-        AddMeta("og:video:width", "640");
-        AddMeta("og:video:height", "360");
-        AddMeta("og:type", "video.other");
-        AddMeta("twitter:card", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
-        AddMeta("twitter:player", $"https://{context.Request.Host}/embed/{Video.Id}");
-        AddMeta("twitter:player:stream", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
-    }
+		AddMeta("description", Video.Description);
+		AddMeta("author", Video.Channel.Title);
+		AddMeta("og:title", Video.Title);
+		AddMeta("og:description", Video.Description);
+		AddMeta("og:url",
+			$"{context.Request.Scheme}://{context.Request.Host}/{context.Request.Path}{context.Request.QueryString}");
+		AddMeta("og:image", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
+		AddMeta("og:video:url", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
+		AddMeta("og:video:width", "640");
+		AddMeta("og:video:height", "360");
+		AddMeta("og:type", "video.other");
+		AddMeta("twitter:card", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
+		AddMeta("twitter:player", $"https://{context.Request.Host}/embed/{Video.Id}");
+		AddMeta("twitter:player:stream", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
+	}
 
-    public WatchContext(HttpContext context, InnerTubePlayer innerTubePlayer, InnerTubeVideo innerTubeVideo,
-        DatabasePlaylist? playlist, ContinuationResponse? comments, bool compatibility, int dislikes, int likes,
-        SponsorBlockSegment[] sponsors) : base(context)
-    {
-        Player = new PlayerContext(context, innerTubePlayer, innerTubeVideo, "embed", compatibility,
-            context.Request.Query["q"], sponsors);
-        Video = innerTubeVideo;
-        Playlist = playlist?.GetVideoPlaylistInfo(innerTubeVideo.Id,
-            DatabaseManager.Users.GetUserFromId(playlist.Author).Result!,
-            DatabaseManager.Playlists.GetPlaylistVideos(playlist.Id, Localization));
-        if (playlist != null && playlist.Visibility == PlaylistVisibility.Private)
-            if (playlist.Author != User?.UserID)
-                Playlist = null;
-        Comments = comments;
-        Dislikes = dislikes;
-        Likes = likes;
-        Sponsors = sponsors;
-        GuideHidden = true;
+	public WatchContext(HttpContext context, InnerTubePlayer innerTubePlayer, InnerTubeVideo innerTubeVideo,
+		DatabasePlaylist? playlist, ContinuationResponse? comments, bool compatibility, int dislikes,
+		SponsorBlockSegment[] sponsors) : base(context)
+	{
+		Player = new PlayerContext(context, innerTubePlayer, innerTubeVideo, "embed", compatibility,
+			context.Request.Query["q"], sponsors);
+		Video = innerTubeVideo;
+		Playlist = playlist?.GetVideoPlaylistInfo(innerTubeVideo.Id,
+			DatabaseManager.Users.GetUserFromId(playlist.Author).Result!,
+			DatabaseManager.Playlists.GetPlaylistVideos(playlist.Id, Localization));
+		if (playlist != null && playlist.Visibility == PlaylistVisibility.Private)
+			if (playlist.Author != User?.UserID)
+				Playlist = null;
+		Comments = comments;
+		Dislikes = dislikes;
+		Likes = -1; // todo: value parsers
+		Sponsors = sponsors;
+		GuideHidden = true;
 
-        AddMeta("description", Video.Description);
-        AddMeta("author", Video.Channel.Title);
-        AddMeta("og:title", Video.Title);
-        AddMeta("og:description", Video.Description);
-        AddMeta("og:url",
-            $"{context.Request.Scheme}://{context.Request.Host}/{context.Request.Path}{context.Request.QueryString}");
-        AddMeta("og:image", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
-        AddMeta("og:video:url", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
-        AddMeta("og:video:width", "640");
-        AddMeta("og:video:height", "360");
-        AddMeta("og:type", "video.other");
-        AddMeta("twitter:card", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
-        AddMeta("twitter:player", $"https://{context.Request.Host}/embed/{Video.Id}");
-        AddMeta("twitter:player:stream", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
+		AddMeta("description", Video.Description);
+		AddMeta("author", Video.Channel.Title);
+		AddMeta("og:title", Video.Title);
+		AddMeta("og:description", Video.Description);
+		AddMeta("og:url",
+			$"{context.Request.Scheme}://{context.Request.Host}/{context.Request.Path}{context.Request.QueryString}");
+		AddMeta("og:image", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
+		AddMeta("og:video:url", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
+		AddMeta("og:video:width", "640");
+		AddMeta("og:video:height", "360");
+		AddMeta("og:type", "video.other");
+		AddMeta("twitter:card", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
+		AddMeta("twitter:player", $"https://{context.Request.Host}/embed/{Video.Id}");
+		AddMeta("twitter:player:stream", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
 
-        AddStylesheet("/lib/ltplayer.css");
+		AddStylesheet("/lib/ltplayer.css");
 
-        AddScript("/lib/ltplayer.js");
-        AddScript("/lib/hls.js");
-        AddScript("/js/player.js");
-    }
+		AddScript("/lib/ltplayer.js");
+		AddScript("/lib/hls.js");
+		AddScript("/js/player.js");
+	}
 
-    public WatchContext(HttpContext context, Exception e, InnerTubeVideo innerTubeVideo, DatabasePlaylist? playlist,
-        ContinuationResponse? comments, int dislikes, int likes) : base(context)
-    {
-        Player = new PlayerContext(context, e);
-        Video = innerTubeVideo;
-        Playlist = playlist?.GetVideoPlaylistInfo(innerTubeVideo.Id,
-            DatabaseManager.Users.GetUserFromId(playlist.Author).Result!,
-            DatabaseManager.Playlists.GetPlaylistVideos(playlist.Id, Localization));
-        if (playlist != null && playlist.Visibility == PlaylistVisibility.Private)
-            if (playlist.Author != User?.UserID)
-                Playlist = null;
-        Comments = comments;
-        Dislikes = dislikes;
-        Likes = likes;
-        Sponsors = [];
-        GuideHidden = true;
+	public WatchContext(HttpContext context, Exception e, InnerTubeVideo innerTubeVideo, DatabasePlaylist? playlist,
+		ContinuationResponse? comments, int dislikes) : base(context)
+	{
+		Player = new PlayerContext(context, e);
+		Video = innerTubeVideo;
+		Playlist = playlist?.GetVideoPlaylistInfo(innerTubeVideo.Id,
+			DatabaseManager.Users.GetUserFromId(playlist.Author).Result!,
+			DatabaseManager.Playlists.GetPlaylistVideos(playlist.Id, Localization));
+		if (playlist != null && playlist.Visibility == PlaylistVisibility.Private)
+			if (playlist.Author != User?.UserID)
+				Playlist = null;
+		Comments = comments;
+		Dislikes = dislikes;
+		Likes = -1; // todo: value parsers
+		Sponsors = [];
+		GuideHidden = true;
 
-        AddMeta("description", Video.Description);
-        AddMeta("author", Video.Channel.Title);
-        AddMeta("og:title", Video.Title);
-        AddMeta("og:description", Video.Description);
-        AddMeta("og:url",
-            $"{context.Request.Scheme}://{context.Request.Host}/{context.Request.Path}{context.Request.QueryString}");
-        AddMeta("og:image", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
-        AddMeta("og:video:url", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
-        AddMeta("og:video:width", "640");
-        AddMeta("og:video:height", "360");
-        AddMeta("og:type", "video.other");
-        AddMeta("twitter:card", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
-        AddMeta("twitter:player", $"https://{context.Request.Host}/embed/{Video.Id}");
-        AddMeta("twitter:player:stream", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
-    }
+		AddMeta("description", Video.Description);
+		AddMeta("author", Video.Channel.Title);
+		AddMeta("og:title", Video.Title);
+		AddMeta("og:description", Video.Description);
+		AddMeta("og:url",
+			$"{context.Request.Scheme}://{context.Request.Host}/{context.Request.Path}{context.Request.QueryString}");
+		AddMeta("og:image", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
+		AddMeta("og:video:url", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
+		AddMeta("og:video:width", "640");
+		AddMeta("og:video:height", "360");
+		AddMeta("og:type", "video.other");
+		AddMeta("twitter:card", $"{context.Request.Scheme}://{context.Request.Host}/proxy/thumbnail/{Video.Id}/-1");
+		AddMeta("twitter:player", $"https://{context.Request.Host}/embed/{Video.Id}");
+		AddMeta("twitter:player:stream", $"https://{context.Request.Host}/proxy/media/{Video.Id}/18.mp4");
+	}
 }
